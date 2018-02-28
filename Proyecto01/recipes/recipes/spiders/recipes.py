c8f714a0a8ecfd2ae0ae635eb4c9feae4cffbb9a
Dr-Tredok <tredok@ciencias.unam.mx>
2018-02-28 16:29:50 -0600

    It now finds recipes, also writes the whole list to a file, properly formatted

--- Proyecto01/recipes/recipes/spiders/recipes.py
+++ Proyecto01/recipes/recipes/spiders/recipes.py
@@ -6,21 +6,25 @@ from scrapy.http import HtmlResponse
 class QuotesSpider(scrapy.Spider):
     name = "recipes"
     body = ''
-    
 
-    
+
+
     def start_requests(self):
         urls = ['https://www.cookingchanneltv.com/recipes/articles/100-traditional-italian-recipes']
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
-            
+
     def parse(self, response):
-        page = response.xpath('//li[contains(@href, "/recipes")]/@href ').extract()
+        # Note that its done with css, and not lxml
+        links = response.css('a[href*=recipes]::attr(href)').extract()
+        # this is just flow controll
         print "AQUI DEBE DE ESTAR LA MIERDA"
-        print page
+        # to actually see if it finds the recipes
+        print links
         print "AQUI TERMINA LA MIERDA"
-        # filename = 'recipes-%s.html' % page
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log('Saved file %s' % filename)
+        # This is all to write the recipes to a file, once they have been found
+        filename = 'ligas-recetas.txt'
+        with open(filename, 'w') as f:
+            for link in links:
+                f.write("%s\n" % link)
 
