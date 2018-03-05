c3bf163a7696941a20f96b45b8de4cbb5ce7f6ab
Dr-Tredok <tredok@ciencias.unam.mx>
2018-03-05 11:07:48 -0600

    Mejoras estéticas al código, se hizo una función para remover las ligas en lugar de 4 for each

--- Proyecto01/recipes/recipes/spiders/recipes.py
+++ Proyecto01/recipes/recipes/spiders/recipes.py
@@ -11,6 +11,15 @@ class QuotesSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+
+    def link(self, links, prefix):
+        recipes = []
+        for link in links:
+            if not link.startswith(prefix):
+                recipes.insert(0, link)
+        return recipes
+        
+
     def parse(self, response):
         # Note that its done with css, and not lxml
         links = response.css('a[href*=recipes]::attr(href)').extract()
@@ -29,25 +38,15 @@ class QuotesSpider(scrapy.Spider):
             if(link.startswith(prefix)):
                 recipes.insert(0, link)
 
-        recipes2 = []
-        for link in recipes:
-            if not link.startswith(prefix2):
-                recipes2.insert(0, link)
-
-        recipes3 = []
-        for link in recipes2:
-            if not link.startswith(prefix3):
-                recipes3.insert(0, link)
-                
-        recipes4 = []
-        for link in recipes3:
-            if not link.startswith(prefix4):
-                recipes4.insert(0, link)
+        # We remove links with prefixes that we do not want.
+        recipes = self.link(recipes, prefix2)
+        recipes = self.link(recipes, prefix3)
+        recipes = self.link(recipes, prefix4)
 
         # Then we write them to a file
         filename = 'ligas-recetas.txt' 
         with open(filename, 'w') as f:
-            for link in recipes4:
+            for link in recipes:
                 f.write("%s\n" % link)
 
         
