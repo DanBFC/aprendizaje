03bcd9dedb87e5348d79ee582933f76fdb4f696c
Daniel Beltrán <d.beltran@gendra.mx>
2018-03-05 17:44:09 -0600

    tmp

--- Proyecto01/recipes/recipes/spiders/recipes.py
+++ Proyecto01/recipes/recipes/spiders/recipes.py
@@ -5,7 +5,7 @@ from scrapy.http import HtmlResponse
 
 class QuotesSpider(scrapy.Spider):
     name = "recipes"
-    
+
     def start_requests(self):
         urls = ['https://www.cookingchanneltv.com/recipes/articles/100-traditional-italian-recipes']
         for url in urls:
@@ -18,7 +18,16 @@ class QuotesSpider(scrapy.Spider):
             if not link.startswith(prefix):
                 recipes.insert(0, link)
         return recipes
-        
+
+    def is_valid(self, link):
+        prefix0 = "//www.cookingchanneltv.com/recipes/" in link
+        prefix1 = "//www.cookingchanneltv.com/recipes/packages/" not in link
+        prefix2 = "//www.cookingchanneltv.com/recipes/photos/" not in link
+        prefix3 = "//www.cookingchanneltv.com/recipes/a-z" not in link
+        return prefix0 and prefix1 and prefix2 and prefix3
+        #prefix4 = "//www.cookingchanneltv.com/recipes/packages/" not in somestring
+        #prefix1 = "//www.cookingchanneltv.com/recipes/packages/" not in somestring
+
 
     def parse(self, response):
         # Note that its done with css, and not lxml
@@ -31,26 +40,22 @@ class QuotesSpider(scrapy.Spider):
         prefix4 = "//www.cookingchanneltv.com/recipes/a-z"
 
         recipes = []
-        
+
         # Now to discriminate the crappy links from the ones we actually want
         # This is an atrocius piece of code, but it works.
         for link in links:
-            if(link.startswith(prefix)):
-                recipes.insert(0, link)
+            if(self.is_valid(link)):
+                recipes.insert(0, link)#.replace("//", "https://"))
+            #if(link.startswith(prefix)):
+            #    recipes.insert(0, link.replace("//", "https://"))
 
         # We remove links with prefixes that we do not want.
-        recipes = self.link(recipes, prefix2)
-        recipes = self.link(recipes, prefix3)
-        recipes = self.link(recipes, prefix4)
+        #recipes = self.link(recipes, prefix2)
+        #recipes = self.link(recipes, prefix3)
+        #recipes = self.link(recipes, prefix4)
 
         # Then we write them to a file
-        filename = 'ligas-recetas.txt' 
+        filename = 'ligas-recetas.txt'
         with open(filename, 'w') as f:
             for link in recipes:
                 f.write("%s\n" % link)
-
-        
-                            
-
-
-    
